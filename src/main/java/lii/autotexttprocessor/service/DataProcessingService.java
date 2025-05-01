9da51149f66ae5f47c5712e12e27b9c030b0909f
Mr Lii <leetamparega@gmail.com>
2025-05-01 02:14:33 +0000

    docs: add analyzeWordFrequency method to return the words and the number of times they appeared in the input teext

--- src/main/java/lii/autotexttprocessor/service/DataProcessingService.java
+++ src/main/java/lii/autotexttprocessor/service/DataProcessingService.java
@@ -1,5 +1,15 @@
 package lii.autotexttprocessor.service;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class DataProcessingService {
+
+    public Map<String, Long> analyzeWordFrequency(String text) {
+        return Arrays.stream(text.split("\\W+"))
+                .filter(word -> !word.isEmpty())
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+    }
 
 }
