46cfd56aef96d66eb6fd7edd521b51c994d4c5ce
Mr Lii <leetamparega@gmail.com>
2025-05-01 03:40:20 +0000

    docs: add configration and settings

--- src/main/java/lii/autotexttprocessor/util/LoggerUtil.java
+++ src/main/java/lii/autotexttprocessor/util/LoggerUtil.java
@@ -1,5 +1,10 @@
-public class LoggingUtil {
-    private static final Logger logger = LogManager.getLogger(LoggingUtil.class);
+package lii.autotexttprocessor.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtil {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
 
     public static void logError(String message, Throwable throwable) {
         logger.error(message, throwable);
