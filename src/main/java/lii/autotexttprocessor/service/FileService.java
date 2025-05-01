ce7286df7995e113bfc1365700e0ea877f5d7d52
Mr Lii <leetamparega@gmail.com>
2025-05-01 01:32:56 +0000

    docs: add readFile and WriteFile methods to FileService

--- src/main/java/lii/autotexttprocessor/service/FileService.java
+++ src/main/java/lii/autotexttprocessor/service/FileService.java
@@ -1,4 +1,25 @@
 package lii.autotexttprocessor.service;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileService {
+
+    public String readFile(String filePath) throws IOException {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append(System.lineSeparator());
+            }
+        }
+        return content.toString();
+    }
+
+    public void writeFile(String filePath, String content) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(filePath))) {
+            writer.write(content);
+        }
+    }
 }
