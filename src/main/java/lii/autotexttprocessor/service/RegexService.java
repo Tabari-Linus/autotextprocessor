bc1fbbf546d8ff9272fd334dc04d0ed57b77d070
Mr Lii <leetamparega@gmail.com>
2025-05-01 01:24:53 +0000

    docs: add searchAndReplace method that takes a string a regex pattern and replacement text and replace the pattern with the text if they match

--- src/main/java/lii/autotexttprocessor/service/RegexService.java
+++ src/main/java/lii/autotexttprocessor/service/RegexService.java
@@ -4,6 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexService {
-
-
+    public String searchAndReplace(String input, String regex, String replacement) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.replaceAll(replacement);
+    }
 }
