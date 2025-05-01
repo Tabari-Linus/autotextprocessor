23aea280a9de502d6c78092fa455169faeb24374
Mr Lii <leetamparega@gmail.com>
2025-05-01 10:50:19 +0000

    docs: add constrcutor, attricbutes, getter and setter functions for the DataEntry class

--- src/main/java/lii/autotexttprocessor/model/DataEntry.java
+++ src/main/java/lii/autotexttprocessor/model/DataEntry.java
@@ -1,4 +1,52 @@
 package lii.autotexttprocessor.model;
 
+import java.util.Objects;
+
 public class DataEntry {
+    private int id;
+    private String name;
+    private String value;
+
+    public DataEntry(int id, String name, String value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DataEntry dataEntry = (DataEntry) o;
+        return id == dataEntry.id && Objects.equals(name, dataEntry.name) && Objects.equals(value, dataEntry.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, value);
+    }
 }
