ee15925f15a3b95b3fbdaf7300a80b8dcff20ab0
Mr Lii <leetamparega@gmail.com>
2025-05-01 09:56:38 +0000

    docs: add findMatch method which returns a list of matched groups from the text that matches the regex pattern

--- src/main/java/lii/autotexttprocessor/util/RegexUtil.java
+++ src/main/java/lii/autotexttprocessor/util/RegexUtil.java
@@ -1,4 +1,21 @@
 package lii.autotexttprocessor.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexUtil {
+
+    public static List<String> findMatches(String text, String regex) {
+        List<String> matches = new ArrayList<>();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+        return matches;
+    }
+
+
 }
