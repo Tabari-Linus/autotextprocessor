e0fafb09050d81107666fc67b1604c33f1de62c2
Mr Lii <leetamparega@gmail.com>
2025-05-01 03:25:47 +0000

    docs: add logError, logInfo, logDebug methods to the LoggingUtil

--- src/main/java/lii/autotexttprocessor/util/LoggerUtil.java
+++ src/main/java/lii/autotexttprocessor/util/LoggerUtil.java
@@ -1,4 +1,15 @@
-package lii.autotexttprocessor.util;
+public class LoggingUtil {
+    private static final Logger logger = LogManager.getLogger(LoggingUtil.class);
 
-public class LoggerUtil {
+    public static void logError(String message, Throwable throwable) {
+        logger.error(message, throwable);
+    }
+
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logDebug(String message) {
+        logger.debug(message);
+    }
 }
