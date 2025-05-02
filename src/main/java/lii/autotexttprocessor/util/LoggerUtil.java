5ff119a572ed46f6e85332e91431440eda523a35
Mr Lii <leetamparega@gmail.com>
2025-05-02 00:49:29 +0000

    docs: modify LoggerUtil to handle logs

--- src/main/java/lii/autotexttprocessor/util/LoggerUtil.java
+++ src/main/java/lii/autotexttprocessor/util/LoggerUtil.java
@@ -1,13 +1,26 @@
 package lii.autotexttprocessor.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.logging.*;
 
 public class LoggerUtil {
-    private static final Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
+    private static final Logger logger = Logger.getLogger(LoggerUtil.class.getName());
+
+    static {
+        try {
+            // Set up the logger
+            LogManager.getLogManager().reset();
+            logger.setLevel(Level.ALL);
+            ConsoleHandler consoleHandler = new ConsoleHandler();;
+            consoleHandler.setLevel(Level.ALL);
+            logger.addHandler(consoleHandler);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static void logError(String message, Throwable throwable) {
-        logger.error(message, throwable);
+        logger.log(Level.SEVERE, message);
     }
 
 
@@ -16,6 +29,10 @@ public class LoggerUtil {
     }
 
     public static void logDebug(String message) {
-        logger.debug(message);
+        logger.log(Level.FINE, message);
+    }
+
+    public static void logWarning(String message) {
+        logger.log(Level.WARNING, message);
     }
 }
