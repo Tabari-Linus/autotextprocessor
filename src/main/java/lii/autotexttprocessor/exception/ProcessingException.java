54f39da9bec174b49b25f0bbe0ee895014cd75a9
Mr Lii <leetamparega@gmail.com>
2025-05-02 01:36:29 +0000

    docs: modify ProcessingException to add custom exception handling for processing text

--- src/main/java/lii/autotexttprocessor/exception/ProcessingException.java
+++ src/main/java/lii/autotexttprocessor/exception/ProcessingException.java
@@ -1,4 +1,32 @@
 package lii.autotexttprocessor.exception;
 
 public class ProcessingException {
+
+    private String message;
+    private Throwable cause;
+
+    public ProcessingException(String message) {
+        this.message = message;
+    }
+
+    public ProcessingException(String message, Throwable cause) {
+        this.message = message;
+        this.cause = cause;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Throwable getCause() {
+        return cause;
+    }
+
+    @Override
+    public String toString() {
+        return "ProcessingException{" +
+                "message='" + message + '\'' +
+                ", cause=" + cause +
+                '}';
+    }
 }
