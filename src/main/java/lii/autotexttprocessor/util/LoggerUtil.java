00259cf102d6b106699c995008595a26bf59d506
Mr Lii <leetamparega@gmail.com>
2025-05-02 11:36:41 +0000

    docs: add documentation to LoggerUtil class

--- src/main/java/lii/autotexttprocessor/util/LoggerUtil.java
+++ src/main/java/lii/autotexttprocessor/util/LoggerUtil.java
@@ -5,7 +5,9 @@ import java.util.logging.*;
 
 public class LoggerUtil {
     private static final Logger logger = Logger.getLogger(LoggerUtil.class.getName());
-
+/**
+ * Static block to configure the logger.
+ */
     static {
         try {
             // Create a FileHandler to log to a file
@@ -30,27 +32,41 @@ public class LoggerUtil {
 
     }
 
+    /**
+     * Logs an error message with a throwable.
+     *
+     * @param message   The error message to log.
+     * @param throwable The throwable to log.
+     */
     public static void logError(String message, Throwable throwable) {
         Logger.getLogger(LoggerUtil.class.getName()).log(Level.SEVERE, message, throwable);
     }
-    public static void logError(String message) {
-        logger.log(Level.SEVERE, message);
-    }
 
-
+    /**
+     * Logs an info message.
+     *
+     * @param message The info message to log.
+     */
     public static void logInfo(String message) {
         logger.info(message);
     }
 
+    /**
+     * Logs a debug message.
+     *
+     * @param message The debug message to log.
+     */
     public static void logDebug(String message) {
         logger.log(Level.FINE, message);
     }
 
+    /**
+     * Logs a warning message.
+     *
+     * @param message The warning message to log.
+     */
     public static void logWarning(String message) {
         logger.log(Level.WARNING, message);
     }
 
-    public static void logError(String errorStartingApplication, String message) {
-        logger.log(Level.SEVERE, errorStartingApplication + ": " + message);
-    }
 }
