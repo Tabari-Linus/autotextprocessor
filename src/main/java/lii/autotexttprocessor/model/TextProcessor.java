4bf441c8d672025133d1e94b9d40b5eed68cf280
Mr Lii <leetamparega@gmail.com>
2025-05-02 01:33:18 +0000

    docs: add method findMatchPattern to return match text

--- src/main/java/lii/autotexttprocessor/model/TextProcessor.java
+++ src/main/java/lii/autotexttprocessor/model/TextProcessor.java
@@ -1,4 +1,25 @@
 package lii.autotexttprocessor.model;
 
+import lii.autotexttprocessor.util.RegexUtil;
+
+import java.util.List;
+
 public class TextProcessor {
+
+    private final RegexUtil regexUtil;
+
+    public TextProcessor() {
+        this.regexUtil = new RegexUtil();
+    }
+
+    // Method to find all matches of a regex pattern in a given text and return them as a list
+    public List<String> findMatches(String text, String regex) {
+        if (!regexUtil.isValidRegexPattern(regex)) {
+            throw new IllegalArgumentException("Not a valid regex pattern: " + regex);
+        }
+        return regexUtil.findMatches(text, regex);
+    }
+
+
+
 }
